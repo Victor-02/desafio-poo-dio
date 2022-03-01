2c5fb10e56c4dcbf51d60c0163e6721e3520ed14
Victor Casemiro <victorcasemiro2002@hotmail.com>
2022-03-01 18:04:18 -0300

    classes criadas

--- curso-java/dio-poo/src/main.java
+++ curso-java/dio-poo/src/main.java
@@ -1,4 +1,15 @@
+import java.time.LocalDate;
 
-public class main {
+public class Main {
+
+	public static void main(String[] args) {
+
+		Curso js = new Curso("Curso JS", "Curso de JavaScript", 8);
+
+		Mentoria ment = new Mentoria("Mentoria JS", "Mentoria de JavaScript", LocalDate.now());
+
+		System.out.println(js);
+		System.out.println(ment);
+	}
 
 }
